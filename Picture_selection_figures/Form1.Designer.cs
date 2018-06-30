4937f4057897f085c3533a48ba077aca72c6f62e
alpaca <36502013+Alpaca-Tech@users.noreply.github.com>
2018-06-30 14:50:46 +0800

    Add files via upload

---
+++ Picture_selection_figures/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Picture_selection_figures
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt_open = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.bt_run = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bt_open
+            // 
+            this.bt_open.Font = new System.Drawing.Font("新細明體", 20F);
+            this.bt_open.Location = new System.Drawing.Point(423, 12);
+            this.bt_open.Name = "bt_open";
+            this.bt_open.Size = new System.Drawing.Size(177, 59);
+            this.bt_open.TabIndex = 0;
+            this.bt_open.Text = "開啟";
+            this.bt_open.UseVisualStyleBackColor = true;
+            this.bt_open.Click += new System.EventHandler(this.bt_open_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(405, 327);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // bt_run
+            // 
+            this.bt_run.Font = new System.Drawing.Font("新細明體", 20F);
+            this.bt_run.Location = new System.Drawing.Point(423, 280);
+            this.bt_run.Name = "bt_run";
+            this.bt_run.Size = new System.Drawing.Size(177, 59);
+            this.bt_run.TabIndex = 2;
+            this.bt_run.TabStop = false;
+            this.bt_run.Text = "框選";
+            this.bt_run.UseVisualStyleBackColor = true;
+            this.bt_run.Click += new System.EventHandler(this.bt_run_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(610, 351);
+            this.Controls.Add(this.bt_run);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.bt_open);
+            this.Name = "Form1";
+            this.Text = "Picture Selection Figures";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt_open;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button bt_run;
+    }
+}
+
---
+++ Picture_selection_figures/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Picture_selection_figures
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bt_open_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog my_open_file_dialog = new OpenFileDialog();
+            my_open_file_dialog.Filter = "Png Image|*.png|JPeg Image|*.jpg|Bitmap Image|*.bmp|GIF Image|*.gif";
+            if (my_open_file_dialog.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = Image.FromFile(my_open_file_dialog.FileName);
+            }
+        }
+
+        private void bt_run_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null) { MessageBox.Show("Error - Not Found Picture!", "Error!"); return; }
+            Bitmap my_bitmap = new Bitmap(pictureBox1.Image);
+
+            int h_l = 0, w_r = 0;        //最低, 最靠右
+            int w_l = my_bitmap.Width;           //最靠左
+            int h_h = my_bitmap.Height;         //最高
+            Color pixel_color = my_bitmap.GetPixel(0, 0);
+
+            for (int i = 0; i < my_bitmap.Width; i++)
+            {
+                for (int j = 0; j < my_bitmap.Height; j++)
+                {
+                    pixel_color = my_bitmap.GetPixel(i, j); //抓取當前像素rgb
+                    if (pixel_color == Color.FromArgb(0, 0, 0) && j < h_h) { h_h = j; } //高
+                    if (pixel_color == Color.FromArgb(0, 0, 0) && j > h_l) { h_l = j; }     //低
+                    if (pixel_color == Color.FromArgb(0, 0, 0) && i < w_l) { w_l = i; }   //左
+                    if (pixel_color == Color.FromArgb(0, 0, 0) && i > w_r) { w_r = i; }       //右
+                }
+            }
+            //MessageBox.Show("最高 " + h_h.ToString() + "\r\n最低 " + h_l.ToString() + "\r\n最左 " + w_l.ToString() + "\r\n最右 " + w_r.ToString());
+            pictureBox1.Image = myPaint(w_l, w_r, h_h, h_l);
+        }
+        
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            
+
+            using (Pen pen = new Pen(Color.Red, 5))
+            {
+                e.Graphics.DrawRectangle(pen, 0, 200, 105, 516);
+            }
+            
+            //左, 右, 高, 低
+            /*if (w_l < 1 || w_r < 1 || h_h < 1 || h_l < 1) { MessageBox.Show("圖片內容貼近邊緣，無法框選！"); return; }
+
+            
+            e.Graphics.DrawLine(new Pen(Color.Red), (w_l - 1), (h_h - 1), (w_r + 1), (h_l + 1));*/
+        }
+
+        private Bitmap myPaint(int w_l, int w_r, int h_h, int h_l)//int w_l, int w_r, int h_h, int h_l
+        {
+
+            Bitmap image = new Bitmap(pictureBox1.Image);
+            Graphics myGraph = Graphics.FromImage(image);
+
+            Rectangle rect = new Rectangle((w_l-1), (h_h-1), (w_r- w_l+2),  (h_l- h_h+2));
+            Pen pen = new Pen(Color.Red);
+            myGraph.DrawRectangle(pen, rect);
+            return image;
+            /*for (int i = h_h; i < h_l; i++)
+            {
+                my_bitmap.SetPixel(w_l-1, i-1, Color.FromArgb(255, 0, 0));
+                my_bitmap.SetPixel(w_r-1, i-1, Color.FromArgb(255, 0, 0));
+            }*/
+
+        }
+    }
+}
